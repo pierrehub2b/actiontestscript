e2d71fe8795ea3733257c414388a4be84767b5b5
pierre huber <pierrehuber@yahoo.fr>
2018-01-17 13:15:07 +0100

    add Atlassian repository

--- target/classes/javascript/checkElementIsVisible.js
+++
@@ -1,3 +0,0 @@
-var element = arguments[0];
-var rect = element.getBoundingClientRect();
-var result = rect.bottom > 0 &&	rect.right > 0 && rect.left < (window.innerWidth || document.documentElement.clientWidth) && rect.top < (window.innerHeight || document.documentElement.clientHeight);
--- target/classes/javascript/getHoverElement.js
+++
@@ -1,7 +0,0 @@
-var e=document.elementFromPoint(arguments[0], arguments[1]);
-var result = null;
-
-if(e){
-	var rect=e.getBoundingClientRect();
-	result = {value:e,tag:e.tagName,x:rect.left+0.00001,y:rect.top+0.00001,width:rect.width+0.00001,height:rect.height+0.00001};
-};
