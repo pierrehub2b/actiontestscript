0fa5147813a38c83e5e12489f6f49e284af60c71
pierre huber <pierrehuber@yahoo.fr>
2018-07-12 18:54:25 +0200

    Javascript micro optimization

--- src/main/resources/javascript/searchElements.js
+++ src/main/resources/javascript/searchElements.js
@@ -1,4 +1,4 @@
-var parent = arguments[0], result = [];
+var parent = arguments[0];
 const tag = arguments[1];
 const attributes = arguments[2];
 const attributesLen = arguments[3];	
@@ -10,7 +10,7 @@ if(parent == null){
 const elts = parent.getElementsByTagName(tag);
 const eltsLength = elts.length;
 
-function addElement(e){
+var result = [], addElement = function (e){
 	let rec = e.getBoundingClientRect();
 	result[result.length] = {'ats-elt':[e, e.tagName, rec.width+0.0001, rec.height+0.0001, rec.left+0.0001, rec.top+0.0001]};
 	return result[result.length-1];
@@ -23,11 +23,11 @@ if(attributesLen == 0){
 	}
 
 }else{
-	
-	for(var i = 0; i < eltsLength; i++){
+
+	var i = 0, loop = function (){
 
 		let e = elts[i], attributesList = [];
-		
+
 		for(var j = 0; j < attributesLen; j++){
 
 			let attributeName = attributes[j];
@@ -35,7 +35,7 @@ if(attributesLen == 0){
 			if(attributeName == 'text'){
 				let textValue = e.textContent;
 				if(textValue){
-					attributesList[attributesList.length] = ['text', textValue.trim().replace(/\xA0/g,' ').replace(/\s+/g,' ')];
+					attributesList[attributesList.length] = ['text', textValue.replace(/\xA0/g,' ').trim()];
 				}
 			}else{
 				if(attributeName == 'checked' && e.tagName == 'INPUT' && (e.type == 'radio' || e.type == 'checkbox')){
@@ -53,12 +53,15 @@ if(attributesLen == 0){
 				}
 			}
 		}
-		
+
 		if(attributesList.length == attributesLen){
 			let newElem = addElement(e);
 			for(var k=0; k < attributesLen; k++){
 				newElem[attributesList[k][0]] = attributesList[k][1];
 			}
 		}
-	}
+
+		i++;
+	};
+	for (; i < eltsLength; ) loop();
 };
