73fed372bf0d98715e46ce2b50f59be3398d1995
pierrehub2b <pierrehuber@yahoo.fr>
2018-07-20 17:22:30 +0200

    Optimize xml images extraction

---
+++ src/main/java/com/ats/recorder/VisualImage.java
@@ -0,0 +1,40 @@
+package com.ats.recorder;
+
+import com.ats.executor.TestBound;
+
+public class VisualImage {
+
+	private String name;
+	private byte[] data;
+	private TestBound bound;
+
+	public VisualImage(String name, byte[] data, TestBound bound) {
+		setName(name);
+		setData(data);
+		setBound(bound);
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public byte[] getData() {
+		return data;
+	}
+
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+		
+	public TestBound getBound() {
+		return bound;
+	}
+
+	public void setBound(TestBound bound) {
+		this.bound = bound;
+	}
+}
