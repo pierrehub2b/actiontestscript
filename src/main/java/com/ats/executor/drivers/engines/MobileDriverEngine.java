a6bf7f7568ee3fd9ea73c185cba8d37f62031e57
Gabriel <contact.gabrielgil@gmail.com>
2019-12-03 17:18:34 +0100

    sending channel bounds for mobile devices

--- src/main/java/com/ats/executor/drivers/engines/MobileDriverEngine.java
+++ src/main/java/com/ats/executor/drivers/engines/MobileDriverEngine.java
@@ -412,7 +412,10 @@ public class MobileDriverEngine extends DriverEngine implements IDriverEngine {
 	
 	@Override
 	public void getScreenshot(TestBound dimension, boolean isRef) {
-		getDesktopDriver().sendRequestCommand(CommandType.Record, RecordType.ImageMobile, this.channel.getApplicationPath()+"/screenshot", isRef); 
+		getDesktopDriver().sendRequestCommand(CommandType.Record, RecordType.ImageMobile,
+				0,0,this.channel.getSubDimension().getWidth().intValue(), this.channel.getSubDimension().getHeight().intValue(),
+				isRef,
+				this.channel.getApplicationPath()+"/screenshot"); 
 	}
 	
 	@Override
