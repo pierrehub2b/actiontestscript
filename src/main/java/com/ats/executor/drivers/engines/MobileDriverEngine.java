b1638d87b86c52445f77f0d489ca51a2e4fe3389
pierrehub2b <pierrehuber@yahoo.fr>
2020-02-25 11:32:48 +0100

    Update MobileDriverEngine.java

--- src/main/java/com/ats/executor/drivers/engines/MobileDriverEngine.java
+++ src/main/java/com/ats/executor/drivers/engines/MobileDriverEngine.java
@@ -421,7 +421,7 @@ public class MobileDriverEngine extends DriverEngine implements IDriverEngine {
 
 	@Override
 	public void clearText(ActionStatus status, TestElement te, MouseDirection md) {	
-		executeRequest(ELEMENT, te.getFoundElement().getId(), TAP, "0", "0");
+		mouseClick(status, te.getFoundElement(), md, 0, 0);
 		executeRequest(ELEMENT, te.getFoundElement().getId(), INPUT, SendKeyData.EMPTY_DATA);
 	}
 	
