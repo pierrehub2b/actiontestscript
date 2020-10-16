7765d3218dd132477d23c42aaf279e06a63de631
Gabriel <contact.gabrielgil@gmail.com>
2020-10-16 11:32:24 +0200

    update stylesheets

--- src/main/resources/reports/campaign/script.js
+++ src/main/resources/reports/campaign/script.js
@@ -2,16 +2,16 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].innerText = "+ " + coll[i].innerText.split('.')[coll[i].innerText.split('.').length-1];
+  coll[i].firstElementChild.innerText = "+ " + coll[i].firstElementChild.innerText.split('.')[coll[i].firstElementChild.innerText.split('.').length-1];
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
-	  this.innerText = this.innerText.replace('-', '+')
+	  this.firstElementChild.innerText = this.firstElementChild.innerText.replace('-', '+')
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-	  this.innerText = this.innerText.replace('+', '-')
+	  this.firstElementChild.innerText = this.firstElementChild.innerText.replace('+', '-')
     } 
   });
 }
