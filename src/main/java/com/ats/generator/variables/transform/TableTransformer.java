ae401a742026a3e7c97e24dca33268acaf386226
pierrehub2b <pierrehuber@yahoo.fr>
2021-05-14 20:13:40 +0200

    Create TableTransformer.java

---
+++ src/main/java/com/ats/generator/variables/transform/TableTransformer.java
@@ -0,0 +1,53 @@
+package com.ats.generator.variables.transform;
+
+import java.util.StringJoiner;
+
+public class TableTransformer extends Transformer {
+	
+	private int[] rows = null;
+	private int random = -1;
+
+	public TableTransformer() {} // Needed for serialization
+
+	public TableTransformer(String ... data) {
+		for(String item : data){
+			
+		}
+	}
+	
+	@Override
+	public String getJavaCode() {
+		
+		final StringJoiner joiner = new StringJoiner(", ");
+		
+		if(random > -1) {
+
+		}
+		
+		if(rows != null) {
+
+		}
+
+		return "(" + joiner.toString() + ")";
+	}
+	
+	//--------------------------------------------------------
+	// getters and setters for serialization
+	//--------------------------------------------------------
+	
+	public int getRandom() {
+		return random;
+	}
+
+	public void setRandom(int random) {
+		this.random = random;
+	}
+	
+	public int[] getRows() {
+		return rows;
+	}
+
+	public void setRow(int[] value) {
+		this.rows = value;
+	}
+}
